debf1c2fb3214dda1579389b328bcc79911ce175
YasutoDATANOMIQ <yasuto.tamura@datanomiq.de>
2023-12-23 21:53:55 +0100

    Changed directory name

--- model_based_RL/tree_search.py
+++ model_based_RL/tree_search.py
@@ -8,6 +8,47 @@ from env_tic_tac_toe import playout
 def argmax(collection, key=None):
     return collection.index(max(collection))
 
+
+# ミニマックス法で状態価値計算
+def mini_max(state):
+    # 負けは状態価値-1
+    if state.is_lose():
+        return -1
+
+    # 引き分けは状態価値0
+    if state.is_draw():
+        return 0
+
+    # 合法手の状態価値の計算
+    best_score = -float('inf')
+    for action in state.legal_actions():
+        score = -mini_max(state.next(action))
+        if score > best_score:
+            best_score = score
+
+    # 合法手の状態価値の最大値を返す
+    return best_score
+
+
+# ミニマックス法で行動選択
+def mini_max_action(state):
+    # 合法手の状態価値の計算
+    best_action = 0
+    best_score = -float('inf')
+    str = ['', '']
+    for action in state.legal_actions():
+        score = -mini_max(state.next(action))
+        if score > best_score:
+            best_action = action
+            best_score = score
+
+        str[0] = '{}{:2d},'.format(str[0], action)
+        str[1] = '{}{:2d},'.format(str[1], score)
+    print('action:', str[0], '\nscore: ', str[1], '\n')
+
+    # 合法手の状態価値の最大値を持つ行動を返す
+    return best_action
+
 # アルファベータ法で状態価値計算
 def alpha_beta(state, alpha, beta):
     # 負けは状態価値-1
