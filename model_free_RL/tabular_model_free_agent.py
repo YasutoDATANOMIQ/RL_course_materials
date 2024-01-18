f0ebcb8aa7a91849c37058854b5e2448afbf8fd5
YasutoDATANOMIQ <yasuto.tamura@datanomiq.de>
2024-01-19 07:57:29 +0900

    Made functions for repetive experiments and comparing logs

--- model_free_RL/tabular_model_free_agent.py
+++ model_free_RL/tabular_model_free_agent.py
@@ -1,4 +1,5 @@
 import itertools
+import random
 from collections import defaultdict
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,12 +8,56 @@ from matplotlib.animation import FuncAnimation
 from PIL import Image
 import io
 
+
+def repetive_experiment(el_agent, experiment_num, episode_count, grid_map_env):
+    reward_log_list = []
+    for i in range(experiment_num):
+        el_agent.learn(grid_map_env, episode_count=episode_count, render=False,)
+        reward_log_list.append(el_agent.reward_log)
+
+    # Calculate mean and standard deviation for each position across arrays
+    reward_average = np.mean(reward_log_list, axis=0)
+    reward_std = np.std(reward_log_list, axis=0)
+
+    return reward_average, reward_std
+
+def compare_experiments(el_agent_list, legend_label_list, experiment_num, episode_count, grid_map_env):
+
+    # Getting random colors
+    # TODO: I want the colors to be more distinguishable
+    colors = plt.get_cmap("tab20").colors
+    colors = list(colors)
+    random.shuffle(colors)
+
+
+
+    plt.figure()
+    for idx, (el_agent, legend_label) in enumerate(zip(el_agent_list, legend_label_list)):
+        print("Conducting experiments of {} for {} episodes, {} times".format(legend_label, episode_count, experiment_num))
+        reward_average, reward_std = repetive_experiment(el_agent, experiment_num, episode_count, grid_map_env)
+
+        plot_color = colors[idx]
+
+        # Plot average
+        plt.plot(reward_average, label=legend_label, color=plot_color)
+
+        # Use fill_between to visualize standard deviation
+        plt.fill_between(range(len(reward_average)),
+                         reward_average - reward_std,
+                         reward_average + reward_std,
+                         alpha=0.1,
+                         color=plot_color,
+                         )
+
+    plt.legend()
+    plt.show()
+
+
 class ELAgent():
+    # TODO: also to generalize action taking processes
 
     def __init__(self, epsilon):
-        # self.Q = {}
         self.epsilon = epsilon
-        self.reward_log = []
         self.history = []
 
     def initialize_Q_value(self, state_space_size, action_space_size, max_reward=None):
@@ -26,11 +71,13 @@ class ELAgent():
         """
 
         if max_reward is None:
-            self.Q = np.zeros((state_space_size, action_space_size))
+            initialized_Q = np.zeros((state_space_size, action_space_size))
         else:
-            self.Q = np.ones((state_space_size, action_space_size))*max_reward
+            initialized_Q = np.ones((state_space_size, action_space_size))*max_reward
 
-    def policy(self, s, actions):
+        return initialized_Q
+
+    def policy(self, s, actions, utility_function='epsilon_greedy'):
         """
         Returns an action given a state.
         TODO: To integrate softmax policy with temperature, action selection with UCB.
@@ -45,7 +92,6 @@ class ELAgent():
         if np.random.random() < self.epsilon:
             return np.random.randint(len(actions))
         else:
-            # if s in self.Q and sum(self.Q[s]) != 0:
             if s < len(self.Q) and sum(self.Q[s])!=0:
                 return np.argmax(self.Q[s])
             else:
@@ -57,7 +103,7 @@ class ELAgent():
     def log(self, reward):
         self.reward_log.append(reward)
 
-    def show_reward_log(self, interval=50, episode=-1):
+    def show_reward_log(self, interval=50):
         """
         Plots a logs of average reward gained with standard deviations
         :param interval:
@@ -65,13 +111,15 @@ class ELAgent():
         :return:
         """
 
-        if episode >0:
-            rewards = self.reward_log[-interval:]
-            mean = np.round(np.mean(rewards), 3)
-            std = np.round(np.std(rewards), 3)
-            print("At Episode {} average reward is {} (+/-{}).".format(
-                   episode, mean, std))
-        else:
+        # TODO: the part below should not be in this function
+        # if episode > 0:
+        #     rewards = self.reward_log[-interval:]
+        #     mean = np.round(np.mean(rewards), 3)
+        #     std = np.round(np.std(rewards), 3)
+        #     print("At Episode {} average reward is {} (+/-{}).".format(
+        #            episode, mean, std))
+
+        if True:
             indices = list(range(0, len(self.reward_log), interval))
             means = []
             stds = []
@@ -84,10 +132,8 @@ class ELAgent():
             plt.figure()
             plt.title("Reward History")
             plt.grid()
-            plt.fill_between(indices, means - stds, means + stds,
-                             alpha=0.1, color="g")
-            plt.plot(indices, means, "o-", color="g",
-                     label="Rewards for each {} episode".format(interval))
+            plt.fill_between(indices, means - stds, means + stds, alpha=0.05, color="g")
+            plt.plot(indices, means, "o-", color="g", label="Rewards for each {} episode".format(interval))
             plt.legend(loc="best")
             plt.show()
 
@@ -95,7 +141,7 @@ class ELAgent():
 
     def extract_episodes_to_animate(self, first_episode_idx, last_episode_idx, episode_interval):
         """
-        # TODO: It might be more efficient to return only indexes of episodes rather thatn history of episode itself.
+        # TODO: It will be more efficient to return only indexes of episodes rather thatn history of episode itself.
         :param first_episode_idx:
         :param last_episode_idx:
         :param episode_interval:
@@ -140,7 +186,6 @@ class ELAgent():
             num_font_size = 10
             ax = self.env.draw_single_grid_map_values(ax, grid_action_value=action_value, grid_agent_state_idx=agent_state)
 
-
             episode_No = episode_number_flattened[frame_idx]
             step_idx = episode_index_flattened[frame_idx]
 
@@ -168,8 +213,8 @@ class QLearningAgent(ELAgent):
         super().__init__(epsilon)
         self.env = env
 
-    def learn(self, env, episode_count=1000, gamma=0.9, learning_rate=0.1, render=False, report_interval=50,
-              max_reward=None):
+    #TODO: To dynamically control the learning rate
+    def learn(self, env, episode_count=1000, gamma=0.9, learning_rate=0.1, render=False, report_interval=50, max_reward=None):
         self.init_log()
 
         try: # First trying to get actions in OpenAI Gym format
@@ -178,32 +223,50 @@ class QLearningAgent(ELAgent):
             actions =  list(range(len(env.actions)))
 
         try: # First trying to get actions in OpenAI Gym format
-            self.initialize_Q_value(env.observation_space.n, env.action_space.n, max_reward=max_reward)
+            self.Q = self.initialize_Q_value(env.observation_space.n, env.action_space.n, max_reward=max_reward)
         except: # Otherwise getting actions in the custom grid map environment formats
-            self.initialize_Q_value(len(self.env.states), len(self.env.actions), max_reward=max_reward)
+            self.Q = self.initialize_Q_value(len(self.env.states), len(self.env.actions), max_reward=max_reward)
+
+
+
 
         for episode_idx in range(episode_count):
             history_one_episode = []
 
+            # env.reset() sometimes returns a state as a tuple
+            # TODO: the case handling belwo might not be the best
             s = env.reset()
             if type(s) == tuple:
                 s = s[0]
 
+            # Appending a history of Q-value
+            # TODO: This migth slower the process, so this should be turned of if animationis not needed
             history_one_episode.append((s, np.copy(self.Q)))
 
             done = False
             while not done:
+
+                # Drawing a step
                 if render:
                     env.render()
+
+                # Taking an action
                 a = self.policy(s, actions)
+                # Getting the next state, reward
                 n_state, reward, done, info, _ = env.step(a)
 
+                # Calculating a TD error
                 gain = reward + gamma * max(self.Q[n_state])
                 estimated = self.Q[s][a]
-                TD_loss = learning_rate * (gain - estimated)
-                self.Q[s][a] += TD_loss
+                TD_loss = gain - estimated
 
+                # Action value update
+                self.Q[s][a] += learning_rate * TD_loss
+
+                # Recording a history
                 history_one_episode.append((n_state, np.copy(self.Q)))
+
+                # Shifting to the next state
                 s = n_state
 
             else:
@@ -211,8 +274,84 @@ class QLearningAgent(ELAgent):
 
             self.history.append(history_one_episode)
 
-            if episode_idx != 0 and episode_idx % report_interval == 0:
-                self.show_reward_log(episode=episode_idx)
+
+class DoubleQLearningAgent(ELAgent):
+    def __init__(self, env, epsilon=0.1):
+        super().__init__(epsilon)
+        self.env = env
 
 
 
+    #TODO: To dynamically control the learning rate
+    def learn(self, env, episode_count=1000, gamma=0.9, learning_rate=0.1, render=False, report_interval=50,
+              max_reward=None):
+        self.init_log()
+        # max_reward = 100
+        try: # First trying to get actions in OpenAI Gym format
+            actions = list(range(env.action_space.n))
+        except: # Otherwise getting actions in the custom grid map environment formats
+            actions =  list(range(len(env.actions)))
+
+        try: # First trying to get actions in OpenAI Gym format
+            self.Q_1 = self.initialize_Q_value(env.observation_space.n, env.action_space.n, max_reward=max_reward)
+            self.Q_2 = self.initialize_Q_value(env.observation_space.n, env.action_space.n, max_reward=max_reward)
+            self.Q = self.Q_1 + self.Q_2
+        except: # Otherwise getting actions in the custom grid map environment formats
+            self.Q_1 = self.initialize_Q_value(len(self.env.states), len(self.env.actions), max_reward=max_reward)
+            self.Q_2 = self.initialize_Q_value(len(self.env.states), len(self.env.actions), max_reward=max_reward)
+            self.Q = self.Q_1 + self.Q_2
+
+        for episode_idx in range(episode_count):
+            history_one_episode = []
+
+            # env.reset() sometimes returns a state as a tuple
+            # TODO: the case handling belwo might not be the best
+            s = env.reset()
+            if type(s) == tuple:
+                s = s[0]
+
+            # Appending a history of Q-value
+            # TODO: This migth slower the process, so this should be turned of if animationis not needed
+            history_one_episode.append((s, np.copy(self.Q)))
+
+            done = False
+            while not done:
+
+                # Drawing a step
+                if render:
+                    env.render()
+
+                # Taking an action
+                a = self.policy(s, actions)
+                # Getting the next state, reward
+                n_state, reward, done, info, _ = env.step(a)
+
+                # In Double Q-learning,
+                if np.random.random() < 0.5:
+                    # Calculating a TD error
+                    action_to_update = np.argmax(self.Q_1[n_state])
+                    gain = reward + gamma * self.Q_2[n_state][action_to_update]
+                    estimated = self.Q_1[s][a]
+                    TD_loss = gain - estimated
+                    # Action value update
+                    self.Q_1[s][a] += learning_rate * TD_loss
+                else:
+                    # Calculating a TD error
+                    action_to_update = np.argmax(self.Q_2[n_state])
+                    gain = reward + gamma * self.Q_1[n_state][action_to_update]
+                    estimated = self.Q_2[s][a]
+                    TD_loss = gain - estimated
+                    # Action value update
+                    self.Q_2[s][a] += learning_rate * TD_loss
+
+                self.Q = self.Q_1 + self.Q_2
+                # Recording a history
+                history_one_episode.append((n_state, np.copy(self.Q)))
+
+                # Shifting to the next state
+                s = n_state
+
+            else:
+                self.log(reward)
+
+            self.history.append(history_one_episode)
